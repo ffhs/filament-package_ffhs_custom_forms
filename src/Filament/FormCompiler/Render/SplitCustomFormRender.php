fa47bf39298199938fd87309b000b30ec6bcb2f4
Kromer Luc <luc.kromer@ffhs.ch>
2024-04-10 15:51:09 +0200

    Modify the methods

--- src/Filament/FormCompiler/Render/SplitCustomFormRender.php
+++ src/Filament/FormCompiler/Render/SplitCustomFormRender.php
@@ -2,61 +2,79 @@
 
 namespace Ffhs\FilamentPackageFfhsCustomForms\Filament\FormCompiler\Render;
 
-use App\Domain\CustomForm\CustomLayoutType\AProductFormSection;
-use Ffhs\FilamentPackageFfhsCustomForms\Filament\FormCompiler\CustomFormRender;
+use Closure;
+use Ffhs\FilamentPackageFfhsCustomForms\CustomField\CustomLayoutType\CustomLayoutType;
 use Ffhs\FilamentPackageFfhsCustomForms\Models\CustomField;
 use Ffhs\FilamentPackageFfhsCustomForms\Models\CustomForm;
 use Ffhs\FilamentPackageFfhsCustomForms\Models\CustomFormAnswer;
-use Filament\Forms\Components\Group;
 
 class SplitCustomFormRender
 {
 
-    public static function renderFormSection(AProductFormSection $section, CustomForm $form, string $viewMode) : array{
-        $sectionField = self::getFields($form, $section);
-
-        if(is_null($sectionField)) return [];
-        $customFields = $sectionField->allCustomFieldsInLayout()->with([
-            "customOptions",
-            "generalField.customOptions",
-            "generalField"
-        ])->get();
-
-        $render= CustomFormRender::getFormRender($viewMode,$form);
-        $customFormSchema = CustomFormRender::render($sectionField->form_position,$customFields,$render,$viewMode)[0];
-
-        return  [
-            Group::make()->schema($customFormSchema)->columnSpanFull()->columns(1),
-        ];
-
+    public static function renderFormLayoutType(CustomLayoutType $layoutType, CustomForm $customForm, string $viewMode = "default") : array{
+        $render= CustomFormRender::getFormRender($viewMode,$customForm);
+        return self::renderLayoutType($layoutType,$customForm,$render,$viewMode);
     }
 
-
-    public static function generateInfoListSchema(AProductFormSection $section,  CustomFormAnswer $formAnswer, string $viewMode):array {
+    public static function renderInfoListLayoutType(CustomLayoutType $layoutType,  CustomFormAnswer $formAnswer, string $viewMode = "default"):array {
         $customForm = CustomForm::cached($formAnswer->custom_form_id);
-        $sectionField = self::getFields($customForm, $section);
-        if (is_null($sectionField)) return [];
-
-        $customFields = $sectionField->allCustomFieldsInLayout()->with([
-            "customOptions",
-            "generalField.customOptions",
-            "generalField"
-        ])->get();
-
         $fieldAnswers = $formAnswer->customFieldAnswers;
 
         $render= CustomFormRender::getInfolistRender($viewMode,$customForm, $formAnswer,$fieldAnswers);
-        return CustomFormRender::render($sectionField->form_position,$customFields,$render,$viewMode)[0];
+
+        return self::renderLayoutType($layoutType,$customForm,$render,$viewMode);
     }
 
 
-    private static function getFields(CustomForm $customForm, AProductFormSection $section): ?CustomField {
+    protected static function renderLayoutType(CustomLayoutType $layoutType,  CustomForm $customForm, Closure $render, string $viewMode = "default"){
         $customFields = $customForm->cachedFields();
 
-        /**@var CustomField $sectionField */
-        $sectionField = $customFields
-            ->where(fn(CustomField $field) => $field->getInheritState()["type"] == $section::getFieldIdentifier())->first();
-        return $sectionField;
+        /**@var null|CustomField $layoutField */
+        $layoutField = $customFields
+            ->filter(fn(CustomField $field) => $field->getInheritState()["type"] == $layoutType::getFieldIdentifier())
+            ->first();
+
+        if (is_null($layoutField)) return [];
+
+        $customFields = $layoutField->allCustomFieldsInLayout()->with([
+            "customOptions",
+            "generalField.customOptions",
+            "generalField"
+        ])->get();
+
+        return CustomFormRender::render($layoutField->form_position,$customFields,$render,$viewMode)[0];
     }
+
+
+    public static function renderFormPose(int $formBeginPos, int $formEndPos, CustomForm $customForm, string $viewMode = "default") : array{
+        $render= CustomFormRender::getFormRender($viewMode,$customForm);
+        return self::renderPose($formBeginPos,$formEndPos,$customForm,$render,$viewMode);
+    }
+
+    public static function renderInfolistPose(int $formBeginPos, int $formEndPos, CustomFormAnswer $formAnswer, string $viewMode  = "default"):array {
+        $customForm = CustomForm::cached($formAnswer->custom_form_id);
+        $fieldAnswers = $formAnswer->customFieldAnswers;
+
+        $render= CustomFormRender::getInfolistRender($viewMode,$customForm, $formAnswer,$fieldAnswers);
+
+        return self::renderPose($formBeginPos,$formEndPos,$customForm,$render,$viewMode);
+    }
+
+
+    protected static function renderPose(int $formBeginPos, int $formEndPos, CustomForm $customForm, Closure $render, string $viewMode  = "default"){
+        $customFields = $customForm
+            ->customFields()
+            ->where("form_position",">=",$formBeginPos)
+            ->where("layout_end_position", "<=", $formEndPos)
+            ->with([
+                "customOptions",
+                "generalField.customOptions",
+                "generalField"
+            ])
+            ->get();
+
+        return CustomFormRender::render($formBeginPos-1,$customFields,$render,$viewMode)[0];
+    }
+
 
 }
