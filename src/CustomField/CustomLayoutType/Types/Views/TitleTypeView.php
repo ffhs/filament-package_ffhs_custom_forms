5a0b538ccd490e203c401eee36f61847d102ed62
Kromer Luc <luc.kromer@ffhs.ch>
2024-05-15 13:07:16 +0200

    Add new templates

--- src/CustomField/CustomLayoutType/Types/Views/TitleTypeView.php
+++ src/CustomField/CustomLayoutType/Types/Views/TitleTypeView.php
@@ -8,7 +8,6 @@ use Ffhs\FilamentPackageFfhsCustomForms\CustomField\View\FieldTypeView;
 use Ffhs\FilamentPackageFfhsCustomForms\Models\CustomField;
 use Ffhs\FilamentPackageFfhsCustomForms\Models\CustomFieldAnswer;
 use Filament\Forms\Components\Placeholder;
-use Filament\Forms\Components\TextInput;
 use Filament\Infolists\Components\TextEntry;
 use Illuminate\Support\HtmlString;
 
@@ -48,10 +47,16 @@ class TitleTypeView implements FieldTypeView
     private static function getTitle($record): string {
         $titleSize = FormMapper::getOptionParameter($record, "title_size");
 
-        if($titleSize == 3) $textClass = 'class="text-xl';
-        else  $textClass = 'class="text-'.(4-$titleSize).'xl"';
+        if($titleSize == 3) $textClass = 'class="text-xl"';
+        else if($titleSize < 3) $textClass = 'class="text-'.(4-$titleSize).'xl"';
+        else {
+            if($titleSize == 4) $textClass = 'class="text-lg"';
+            else if($titleSize == 5) $textClass = 'class="text-base"';
+            else if($titleSize == 6) $textClass = 'class="text-sm"';
+            else $textClass = 'class="text-xs"';
+        }
         $titleText = FormMapper::getLabelName($record);
-        return '<h'.$titleSize.' '.$textClass.'>'.$titleText.'<h'.$titleSize.'> ';
+        return '<h'.$titleSize.' '.$textClass.'>'.$titleText.' </h'.$titleSize.'> ';
     }
 
 }
