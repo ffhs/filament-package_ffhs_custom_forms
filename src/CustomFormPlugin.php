18a2d7df98927acf77b0b4dc9496bb6269c7fca6
Christoph Karlen <christoph.karlen@ffhs.ch>
2024-07-31 17:38:19 +0200

    Add policies

--- src/CustomFormPlugin.php
+++ src/CustomFormPlugin.php
@@ -2,12 +2,17 @@
 
 namespace Ffhs\FilamentPackageFfhsCustomForms;
 
+use Ffhs\FilamentPackageFfhsCustomForms\Policies\CustomFormAnswerPolicy;
+use Ffhs\FilamentPackageFfhsCustomForms\Policies\CustomFormPolicy;
+use Ffhs\FilamentPackageFfhsCustomForms\Policies\GeneralFieldPolicy;
 use Ffhs\FilamentPackageFfhsCustomForms\Resources\CustomFormAnswerResource;
 use Ffhs\FilamentPackageFfhsCustomForms\Resources\CustomFormResource;
 use Ffhs\FilamentPackageFfhsCustomForms\Resources\GeneralFieldResource;
 use Ffhs\FilamentPackageFfhsCustomForms\Resources\TemplateResource;
+use Ffhs\FilamentPackageFfhsSso\Models\SsoUser;
 use Filament\Contracts\Plugin;
 use Filament\Panel;
+use Illuminate\Support\Facades\Gate;
 
 class CustomFormPlugin implements Plugin {
 
@@ -31,6 +36,13 @@ class CustomFormPlugin implements Plugin {
             ]);
     }
 
+
+    public function packageBooted(): void {
+        //Setup Policies
+        Gate::policy(SsoUser::class, CustomFormPolicy::class);
+        Gate::policy(SsoUser::class, CustomFormAnswerPolicy::class);
+        Gate::policy(SsoUser::class, GeneralFieldPolicy::class);
+    }
 
     public function boot(Panel $panel): void {
 
---
+++ src/Policies/GeneralFieldPolicy.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Ffhs\FilamentPackageFfhsCustomForms\Policies;
+
+use App\Models\User;
+use Ffhs\FilamentPackageFfhsCustomForms\Models\GeneralField;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class GeneralFieldPolicy
+{
+    use HandlesAuthorization;
+
+    public function viewAny(User $user): bool
+    {
+        return $user->can('view_any_general_field') ?? false;
+    }
+
+    public function view(User $user, GeneralField $generalField): bool
+    {
+        return $user->can('view_general_field') ?? false;
+    }
+
+    public function update(User $user, GeneralField $generalField): bool
+    {
+        return $user->can('update_general_field') ?? false;
+    }
+
+    public function create(User $user, GeneralField $generalField): bool
+    {
+        return $user->can('create_general_field') ?? false;
+    }
+
+    public function delete(User $user, GeneralField $generalField): bool
+    {
+        return $user->can('delete_general_field') ?? false;
+    }
+}
