f6e00ea6929dc482696aa38812dc54eb4866df36
Luc Kromer <luc.kromer@ffhs.ch>
2024-08-29 16:53:22 +0200

    Fixing translations

--- src/CustomField/CustomFieldType/LayoutType/Types/Views/TextLayoutTypeView.php
+++ src/CustomField/CustomFieldType/LayoutType/Types/Views/TextLayoutTypeView.php
@@ -2,6 +2,7 @@
 
 namespace Ffhs\FilamentPackageFfhsCustomForms\CustomField\CustomFieldType\LayoutType\Types\Views;
 
+use Barryvdh\Debugbar\Facades\Debugbar;
 use Ffhs\FilamentPackageFfhsCustomForms\CustomField\CustomFieldType\GenericType\CustomFieldType;
 use Ffhs\FilamentPackageFfhsCustomForms\CustomField\CustomFieldType\GenericType\FieldTypeView;
 use Ffhs\FilamentPackageFfhsCustomForms\CustomField\FieldMapper;
@@ -15,10 +16,9 @@ use Illuminate\Support\HtmlString;
 class TextLayoutTypeView implements FieldTypeView
 {
 
-    public static function getFormComponent(CustomFieldType $type, CustomField $record,
-                                            array           $parameter = []): Placeholder {
+    public static function getFormComponent(CustomFieldType $type, CustomField $record, array  $parameter = []): Placeholder {
 
-        $text = FieldMapper::getOptionParameter($record,"text_de"); //ToDo translate
+        $text = FieldMapper::getOptionParameter($record,"text")[app()->getLocale()] ?? "";
 
         return  Placeholder::make(FieldMapper::getIdentifyKey($record))
             ->columnStart(FieldMapper::getOptionParameter($record,"new_line_option"))
@@ -28,14 +28,13 @@ class TextLayoutTypeView implements FieldTypeView
             ->label("");
     }
 
-    public static function getInfolistComponent(CustomFieldType $type, CustomFieldAnswer $record,
-                                                array           $parameter = []): Component {
+    public static function getInfolistComponent(CustomFieldType $type, CustomFieldAnswer $record, array   $parameter = []): Component {
 
         if(!FieldMapper::getOptionParameter($record,"show_in_view"))
             return \Filament\Infolists\Components\Group::make()->hidden();
 
         $label = FieldMapper::getOptionParameter($record,"show_title")? FieldMapper::getLabelName($record):"";
-        $text = FieldMapper::getOptionParameter($record,"text_de"); //ToDo translate
+        $text = FieldMapper::getOptionParameter($record,"text")[app()->getLocale()] ?? "";
 
         return TextEntry::make(FieldMapper::getIdentifyKey($record))
             ->columnStart(FieldMapper::getOptionParameter($record,"new_line_option"))
