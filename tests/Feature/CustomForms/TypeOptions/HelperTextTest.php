1e88b404bb3e7d5cd06b5ec9a6e3019645c246d1
Luc Kromer <luc.kromer@ffhs.ch>
2025-04-02 15:07:45 +0200

    improve Test

--- tests/Feature/CustomForms/TypeOptions/HelperTextTest.php
+++ tests/Feature/CustomForms/TypeOptions/HelperTextTest.php
@@ -100,7 +100,7 @@ test('field has helper text in component', function ($customFieldIdentifier, arr
         expect($component->getHelperText())->toBeNull();
     };
 
-    $checkOptionFunction = function (Component $component) use ($helpText) {
+    $checkOptionFunction = function (Component|HasHelperText $component) use ($helpText) {
         expect($component->getHelperText())->not()->toBeNull()
             ->and($component->getHelperText())->toBeInstanceOf(HtmlString::class)
             ->and($component->getHelperText())->toEqual($helpText);
