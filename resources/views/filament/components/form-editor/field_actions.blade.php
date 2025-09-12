ba76d63fd122bfebe01fbba272ef93b863bc637a
Luc Kromer <luc.kromer@ffhs.ch>
2025-09-12 11:06:27 +0200

    Fixing js error

--- resources/views/filament/components/form-editor/field_actions.blade.php
+++ resources/views/filament/components/form-editor/field_actions.blade.php
@@ -1,10 +1,4 @@
-@php
-    use Filament\Support\Facades\FilamentAsset;
-@endphp
-
 <div
-    x-load
-    x-load-css="['{{FilamentAsset::getStyleHref('custom_forms', package: 'ffhs/filament-package_ffhs_custom-forms')}}']"
     class="-mt-5 -mr-1">
     @include('filament-schemas::components.actions')
 </div>
--- resources/views/filament/components/form-editor/index.blade.php
+++ resources/views/filament/components/form-editor/index.blade.php
@@ -1,12 +1,14 @@
-<span class="hidden"
-      x-load
-      x-load-css="{{\Filament\Support\Facades\FilamentAsset::getStyleHref('custom_forms', 'ffhs/filament-package_ffhs_custom-forms')}}"
-> </span>
+@php use Filament\Support\Facades\FilamentAsset; @endphp
+
 
 <x-dynamic-component
     :component="$getFieldWrapperView()"
     :field="$field"
     class="items-stretch"
 >
+    <span class="hidden"
+          x-load
+          x-load-css="['{{FilamentAsset::getStyleHref('custom_forms', package: 'ffhs/filament-package_ffhs_custom-forms')}}']"
+    > </span>
     {{ $getChildSchema() }}
 </x-dynamic-component>
