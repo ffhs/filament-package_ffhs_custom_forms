d79038082628e7b8fee098788d5b7bef39725fcc
Luc Kromer <luc.kromer@ffhs.ch>
2025-10-24 11:15:58 +0200

    fix stan

--- src/Filament/Component/FormEditor/Field/EditField.php
+++ src/Filament/Component/FormEditor/Field/EditField.php
@@ -4,13 +4,13 @@ namespace Ffhs\FilamentPackageFfhsCustomForms\Filament\Component\FormEditor\Fiel
 
 
 use Ffhs\FfhsUtils\Filament\Components\ElevatedActions;
-use Ffhs\FilamentPackageFfhsCampus\Filament\Resources\Products\RelationManagers\PasserelleProductsRelationManager;
 use Ffhs\FilamentPackageFfhsCustomForms\CustomFieldType\GenericType\CustomFieldType;
 use Ffhs\FilamentPackageFfhsCustomForms\Facades\CustomForms;
 use Ffhs\FilamentPackageFfhsCustomForms\Models\CustomField;
 use Ffhs\FilamentPackageFfhsCustomForms\Traits\HasFormConfiguration;
 use Filament\Forms\Components\TextInput;
 use Filament\Pages\Page;
+use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Schemas\Components\Component;
 
 class EditField extends Component
@@ -32,7 +32,7 @@ class EditField extends Component
         return CustomForms::getFieldTypeFromRawDate($state, $this->getFormConfiguration());
     }
 
-    public function getFieldComponents($state, Page|PasserelleProductsRelationManager $livewire): array
+    public function getFieldComponents($state, Page|RelationManager $livewire): array
     {
         $type = $this->getFieldType($state);
         $localAdding = $livewire->getActiveSchemaLocale() ?? app()->getLocale();
